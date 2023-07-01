19f0a53e0c1764158d963729b5607c6921284c21
cango91 <can.gologlu@gmail.com>
2023-07-01 17:14:12 -0400

    mess with class structure

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,13 @@
 
+import Card, {Suit} from './card.js';
+
 /* DEBUGGING TOOLS */
 window.ConsoleObject = {}
+window.ConsoleObject.Card = Card;
+window.ConsoleObject.Suit = Suit;
+window.ConsoleObject.makeCard = function (arg){
+    return new Card(arg);
+}
 window.ConsoleObject.tryFlipping = function (card, cardClass, oldClass = 'back') {
     card.classList.add('flipping-first-half');
     setTimeout(function () {
@@ -14,13 +21,13 @@ window.ConsoleObject.tryFlipping = function (card, cardClass, oldClass = 'back')
     }, 250);
 }
 
-window.ConsoleObject.tryDealing = function (numTableau,speed = 2500,dealingFrom='#deck-slot') { /* speed in px/sec */
+window.ConsoleObject.tryDealing = function (numTableau, speed = 2500, dealingFrom = '#deck-slot') { /* speed in px/sec */
     // get top card from deck
     const card = document.querySelector('.card.deck:last-child');
-    const slot = document.querySelector(`#tableau-${numTableau}`);
+    const slot = document.querySelector(`${typeof numTableau === 'number' ? `#tableau-${numTableau}` : numTableau}`);
     const dealingSlot = document.querySelector(dealingFrom);
     let slotRect = slot.firstElementChild ? slot.lastElementChild.getBoundingClientRect() : slot.getBoundingClientRect();
-    
+
     const cardRect = card.getBoundingClientRect();
     let initialTop = cardRect.top;
     let initialLeft = cardRect.left;
@@ -35,18 +42,28 @@ window.ConsoleObject.tryDealing = function (numTableau,speed = 2500,dealingFrom=
         { transform: 'translate(0, 0)' },
         // final state (offset from the card's position to the slot's position)
         { transform: `translate(${slotRect.left - initialLeft}px, ${slotRect.top - initialTop}px)` }
-      ];
+    ];
 
-    let duration = Math.sqrt((slotRect.left - initialLeft)**2 + (slotRect.top - initialTop)**2)/speed*1000;
+    let duration = Math.sqrt((slotRect.left - initialLeft) ** 2 + (slotRect.top - initialTop) ** 2) / speed * 1000;
 
     let options = {
         duration: duration,
         fill: 'forwards'
     };
-    card.animate(keyframes, options).onfinish=()=>{
+    const newCard = document.createElement('div');
+    newCard.classList.add('card', 'large', 'on-tableau', 'back');
+    card.animate(keyframes, options).onfinish = () => {
         dealingSlot.removeChild(card);
-        const newCard = document.createElement('div');
-        newCard.classList.add('card','large','on-tableau','back');
         slot.append(newCard);
     };
+    return newCard;
+}
+
+window.ConsoleObject.tryWaste = function (num = 1) {
+    for (let i = 0; i < num; i++) {
+        const card = window.ConsoleObject.tryDealing("#waste-slot");
+        card.classList.remove('on-tableau');
+        card.classList.add('on-waste');
+        window.ConsoleObject.tryFlipping(card, 'dA');
+    }
 }
